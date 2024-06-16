c4366b4e9174a1b3b5171d937deccae01595342e
Ron Wexler <ronmada@gmail.com>
2024-06-16 11:08:21 +0300

    reformat and tests

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
@@ -6,8 +6,15 @@ import { RouterOutlet } from '@angular/router';
   standalone: true,
   imports: [RouterOutlet],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'proj1';
+  private aa = signal('a');
+  ngOnInit(): void {
+    this.aaa();
+  }
+  public aaa() {
+    console.log('🚀 ~ AppComponent ~ aa:', this.aa());
+  }
 }
